3da583b4ff579d4e46436667ca8fc9c4003e2570
ssjaweid <132857427+ssjaweid@users.noreply.github.com>
2023-10-26 19:40:46 -0400

    Adding Hazel's code

--- combined_cc.py
+++ combined_cc.py
@@ -1,55 +1,133 @@
 import streamlit as st
+import pandas as pd
+import math
+import requests
+
+# Function to calculate distance between cities using Google Maps API
+def calculate_distance(from_city, to_city, api_key):
+    base_url = 'https://maps.googleapis.com/maps/api/distancematrix/json'
+    params = {
+        'origins': from_city,
+        'destinations': to_city,
+        'key': api_key,
+    }
+    response = requests.get(base_url, params=params)
+    data = response.json()
+    if data.get('status') == 'OK':
+        try:
+            distance_text = data['rows'][0]['elements'][0]['distance']['text']
+            return distance_text
+        except KeyError:
+            return "Distance data not found"
+    else:
+        return "Invalid city name(s)"
+
+# Function to calculate haversine distance
+def haversine_distance(lat1, lon1, lat2, lon2):
+    earth_radius = 3958.8
+    lat1, lon1, lat2, lon2 = map(math.radians, [lat1, lon1, lat2, lon2])
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+    a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    distance = earth_radius * c
+    return distance
 
 # Create a Streamlit web app
-st.title("Car Consumption and Carbon Footprint Calculator")
+st.title("Carbon Footprint Calculator")
 
-# User inputs
-car_type = st.selectbox("Select Car Type", ["Gas", "Hybrid", "Electric"])
+# Car Calculator
+st.subheader("Car Carbon Footprint Calculator")
+car_type = st.radio("Select Car Type", ["Gas", "Hybrid", "Electric"])
 miles_driven = st.number_input("Miles Driven", min_value=0, value=5000)
 fuel_efficiency = st.number_input("Fuel Efficiency (mpg or eMPG)", min_value=0, value=6)
 
-# Function to calculate emissions factor (in kg CO2e per mile)
-def calculate_emissions_factor(miles_traveled, emissions_factor_per_mile):
-    emissions_factor = (emissions_factor_per_mile / 1000) * miles_traveled
-    return emissions_factor
+# Car Carbon Footprint Calculation
+if st.button("Calculate Car Carbon Footprint"):
+    emissions_factor_per_mile = 0.42  # Example emissions factor (kg CO2e per mile)
+    emissions_factor_per_km = 0.621371 * emissions_factor_per_mile  # Convert to kg CO2e per km
 
-# Input values for the emissions factor calculation (example values)
-emissions_factor_per_mile = 0.42  # Emissions factor per mile (kg CO2e/mi)
+    if car_type == "Electric":
+        energy_consumption = miles_driven / fuel_efficiency
 
-# Calculate emissions factor
-emissions_factor = calculate_emissions_factor(miles_driven, emissions_factor_per_mile)
+        carbon_emissions_miles = miles_driven * emissions_factor_per_mile
+        carbon_emissions_km = energy_consumption * emissions_factor_per_km
 
-# Calculate gas or energy consumption and carbon footprint based on car type
-if car_type == "Electric":
-    energy_consumption = miles_driven / fuel_efficiency
-    st.write(f"Energy Consumption: {energy_consumption:.2f} kWh")
+        st.write(f"Energy Consumption: {energy_consumption:.2f} kWh")
+        st.write(f"Carbon Footprint (Miles): {carbon_emissions_miles:.2f} kg CO2e")
+        st.write(f"Carbon Footprint (Kilometers): {carbon_emissions_km:.2f} kg CO2e")
 
-    # Calculate carbon emissions for electric cars in kgCO2e per mile using the emissions factor
-    carbon_emissions = energy_consumption * emissions_factor
+    elif car_type == "Gas" or car_type == "Hybrid":
+        consumption = miles_driven / fuel_efficiency
 
-    st.write(f"Carbon Footprint: {carbon_emissions:.2f} kgCO2e")
+        carbon_emissions_miles = miles_driven * emissions_factor_per_mile
+        carbon_emissions_km = consumption * emissions_factor_per_km
+
+        st.write(f"Fuel Consumption: {consumption:.2f} gallons")
+        st.write(f"Carbon Footprint (Miles): {carbon_emissions_miles:.2f} kg CO2e")
+        st.write(f"Carbon Footprint (Kilometers): {carbon_emissions_km:.2f} kg CO2e")
+
+# Rail/Bus Calculator
+st.subheader("Rail/Bus Carbon Footprint Calculator")
+transportation_type = st.selectbox("Select transporation type", ["Rail", "Bus"])
+from_city = st.text_input("From City", "")
+to_city = st.text_input("To City", "")
+google_api_key = 'AIzaSyCIx-_G27hwPK97r6B_b4FyF4sTxHq7qWw'
+# Rail/Bus Carbon Footprint Calculation
+if st.button("Calculate Rail/Bus Carbon Footprint"):
+    api_key = 'AIzaSyCIx-_G27hwPK97r6B_b4FyF4sTxHq7qWw'
+distance = calculate_distance(from_city, to_city, google_api_key)
+
+if "Invalid" not in distance:
+    distance_km = float(distance.split()[0])
+    distance_miles = distance_km * 0.621371
+    distance_text = calculate_distance(from_city, to_city, google_api_key)
     
-    # Calculate carbon offset (assuming 100% renewable energy source)
-    carbon_offset = carbon_emissions
-    st.write(f"Carbon Offset: {carbon_offset:.2f} kgCO2e")
+    if transportation_type == "Rail":
+            emissions_factor_per_mile = 0.46
+            miles_traveled = float(distance_miles)
+            trans_carbon = emissions_factor_per_mile * miles_traveled
+            st.success(f"Carbon Footprint for Rail: {trans_carbon:.2f} kgCO2e")
+    elif transportation_type == "Bus":
+            emissions_factor_per_mile = 0.4
+            miles_traveled = float(distance_miles)
+            trans_carbon = emissions_factor_per_mile * miles_traveled
+            st.success(f"Carbon Footprint for Bus: {trans_carbon:.2f} kgCO2e")
 
-elif st.button("Calculate Now"):
-    if car_type == "Gas":
-        gas_consumption = miles_driven / fuel_efficiency
-        st.write(f"Gas Consumption: {gas_consumption:.2f} gallons")
+    st.write(f"Distance: {distance}")
+    st.write(f"Carbon Footprint (Miles): {trans_carbon:.2f} kg CO2e")
+else:
+    st.error("Invalid city name(s)")
 
-        # Average carbon emissions for gas cars in kgCO2e per mile
-        carbon_emissions = gas_consumption * emissions_factor
+# Air Miles Calculator
+st.subheader("Air Miles Carbon Footprint Calculator")
+from_iata = st.text_input("From IATA Code", "").strip().upper()
+to_iata = st.text_input("To IATA Code", "").strip().upper()
 
-        st.write(f"Carbon Footprint: {carbon_emissions:.2f} kgCO2e")
+# Air Miles Carbon Footprint Calculation
+if st.button("Calculate Air Miles Carbon Footprint"):
+    airport_data = pd.read_csv('iata-icao.csv')
 
-    elif car_type == "Hybrid":
-        hybrid_consumption = miles_driven / fuel_efficiency
-        st.write(f"Hybrid Consumption: {hybrid_consumption:.2f} gallons")
+    lat1, lon1 = 40.7128, -74.0060
+    lat2, lon2 = 34.0522, -118.2437  
 
-        # Average carbon emissions for hybrid cars in kgCO2e per mile
-        carbon_emissions = hybrid_consumption * emissions_factor
-        st.write(f"Carbon Footprint: {carbon_emissions:.2f} kgCO2e")
+    # Calculate the air miles distance
+    distance = haversine_distance(lat1, lon1, lat2, lon2)
+    st.write(f"Air miles distance: {distance:.2f} miles")
 
-# Information about fuel efficiency and carbon emissions data source
-st.info("Please note that the fuel efficiency and carbon emissions data should be based on real-world figures and specific to the car type you are using. You can obtain this data from trusted sources.")
+    # Look up coordinates for the provided IATA codes
+    from_airport = airport_data[airport_data['iata'] == from_iata]
+    to_airport = airport_data[airport_data['iata'] == to_iata]
+
+    if not from_airport.empty and not to_airport.empty:
+        lat1, lon1 = from_airport['latitude'].values[0], from_airport['longitude'].values[0]
+        lat2, lon2 = to_airport['latitude'].values[0], to_airport['longitude'].values[0]
+
+        distance = haversine_distance(lat1, lon1, lat2, lon2)
+        emissions_factor = 1.47
+        air_miles_carbon = distance * emissions_factor
+
+        st.write(f"Flight Distance: {distance:.2f} miles")
+        st.write(f"Carbon Footprint (Miles): {air_miles_carbon:.2f} kg CO2e")
+    else:
+        st.error("Invalid IATA codes. Please provide valid IATA codes")
